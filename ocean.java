e4b2c5a69916d6c870d1e9fd4ba882e0c2404bdc
ARNAUD <arnaud@duhamel.biz>
2020-04-19 22:49:38 +0200

    Test GitHub

--- ocean.java
+++ ocean.java
@@ -1,5 +1,6 @@
 //TODO : Manage the possibility to move before fire a torpedo or mine.
 //TODO : Manage the "I loose life" for TRIGGER and not only for TORPEDO
+//Imported in GitHub
 
 import java.util.*;
 import java.io.*;
