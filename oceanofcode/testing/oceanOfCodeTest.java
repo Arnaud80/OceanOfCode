b50c4d1c4962abf8c39464cc452937ebca34ecad
ARNAUD <arnaud@duhamel.biz>
2020-04-27 18:57:59 +0200

    Junit integration

---
+++ oceanofcode/testing/oceanOfCodeTest.java
@@ -0,0 +1,14 @@
+package oceanofcode.testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class oceanOfCodeTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
